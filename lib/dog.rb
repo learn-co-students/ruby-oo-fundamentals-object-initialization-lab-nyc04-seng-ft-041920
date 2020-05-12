b2f91aa6433b638d18911f47414d8942f86a3137
Dell Wilson <warren.wilsonjr89@gmail.com>
2020-05-12 20:03:44 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,8 +4,26 @@ class Dog
     @breed = breed 
   end
   
-#   def new_breed
-#     breed = "Mutt"
-
+#   class Dog 
+# attr_accessor :breed 
+# def initialize 
+#   @breed = "Mutt"
 # end 
+# end 
+
+# d1 = Dog.new 
+# d1.breed 
+
+
+#   def breed
+#     @breed
+#   end 
+  
+#   def breed
+#     @breed = "Mutt"
+#   end 
+
+# fido = Dog.new("Mutt")
+# fido.breed
+
 end 
